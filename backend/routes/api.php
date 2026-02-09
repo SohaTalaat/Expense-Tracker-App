e85873281eca1bd8ace8240f21f59e16f93c3701
Soha <saha.20350436@science.aun.edu.eg>
2026-02-09 21:39:04 +0200

    Update routes - Add global style

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,8 +11,8 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('me', [AuthController::class], 'me');
-    Route::post('logout', [AuthController::class], 'logout');
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
 
     Route::apiResource('categories', categoryController::class);
     Route::apiResource('expenses', ExpenseController::class);
